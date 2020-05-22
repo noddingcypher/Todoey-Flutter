7f780717c5f386515113e90d5c1c4dae06111813
noddingcypher <kangsj24@gmail.com>
2020-05-22 19:06:29 +0900

    TOdoey Flutter

---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
+import 'package:todoeyflutter/models/task_data.dart';
+import 'package:todoeyflutter/widgets/task_tile.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        //
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkBoxCallback: (checkBoxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                //
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
